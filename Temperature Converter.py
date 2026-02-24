9b0d4e08b13c99d28080a3b1ba13bb58cb45ee48
Srujan-1-2 <srujankathi6@gmail.com>
2026-02-24 15:15:15 +0530

    Add files via upload

---
+++ Temperature Converter.py
@@ -0,0 +1,37 @@
+print("Temperature Converter")
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
+print("3. Celsius to Kelvin")
+print("4. Kelvin to Celsius")
+print("5. Fahrenheit to Kelvin")
+print("6. Kelvin to Fahrenheit")
+print("7. Exit")
+choice = int(input("Enter your choice: "))
+if choice == 1:
+    c = float(input("Enter Celsius: "))
+    f = (c * 9/5) + 32                   
+    print("Fahrenheit =", f)
+elif choice == 2:
+    f = float(input("Enter Fahrenheit: "))
+    c = (f - 32) * 5/9                   
+    print("Celsius =", c)
+elif choice == 3:
+    c = float(input("Enter Celsius: "))
+    k = c + 273.15                        
+    print("Kelvin =", k)
+elif choice == 4:
+    k = float(input("Enter Kelvin: "))
+    c = k - 273.15                       
+    print("Celsius =", c)
+elif choice == 5:
+    f = float(input("Enter Fahrenheit: "))
+    k = (f - 32) * 5/9 + 273.15          
+    print("Kelvin =", k)
+elif choice == 6:
+    k = float(input("Enter Kelvin: "))
+    f = (k - 273.15) * 9/5 + 32          
+    print("Fahrenheit =", f)
+elif choice == 7:
+    print("Program Ended")
+else:
+    print("Invalid choice")
